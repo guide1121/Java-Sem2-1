e1e358fb613f4dc9614754b758917e1f3dd6d068
Naphat Phoruang <99393112+guide1121@users.noreply.github.com>
2025-02-02 23:25:40 +0700

    Lab05

--- Test2.java
+++ Test2.java
@@ -1,11 +1,13 @@
 public class Test2 {
     public static void main(String[] args) {
 
-        int y = 10;
-        int x = y++;
-        
-        System.out.println(x);
-        System.out.println(y);
+       double[] data = new double[10];
+        System.out.println(data[1]);
+    //    for(int i = 0; i< data.length; i++){
+    //     data[i] = i * i;
+    //    }
+    //    System.out.println(data);
+
 
     }
 }
